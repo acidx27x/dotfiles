5c3875c5c7502e0bcbd5db0e417d49cd6fbf9e33
acidx27x <telegram@acidx27x>
2025-10-20 02:00:14 +0300

    * nvim: (plugins, mini) add more words to hipatterns

--- .config/nvim/lua/plugins/mini-hipatterns-config.lua
+++ .config/nvim/lua/plugins/mini-hipatterns-config.lua
@@ -6,9 +6,16 @@ hipatterns.setup({
   highlighters = {
     -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
     fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    error = { pattern = '%f[%w]()ERROR()%f[%W]', group = 'MiniHipatternsFixme' },
+
     hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+    warn  = { pattern = '%f[%w]()WARN()%f[%W]',  group = 'MiniHipatternsHack'  },
+
     todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+
     note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+    hint  = { pattern = '%f[%w]()HINT()%f[%W]',  group = 'MiniHipatternsNote'  },
+    info  = { pattern = '%f[%w]()INFO()%f[%W]',  group = 'MiniHipatternsNote'  },
 
     -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = hipatterns.gen_highlighter.hex_color(),
