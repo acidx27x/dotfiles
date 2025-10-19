a0093fa3d9cfa0a01fad9edaa1fbee19f990f2c0
acidx27x <telegram@acidx27x>
2025-10-20 02:00:58 +0300

    * nvim: (plugins, navic) remove unused code

--- .config/nvim/lua/plugins/nvim-navic-config.lua
+++ .config/nvim/lua/plugins/nvim-navic-config.lua
@@ -52,55 +52,10 @@ function M.get()
 end
 
 
---local cache = {
---  text = "",      -- what winbar displays
---  updating = false,
---}
---
---function M.update_async()
---  if cache.updating then return end
---  cache.updating = true
---
---  vim.schedule(function()
---    -- do heavy computation or async stuff here
---    local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
---    if not wininfo then cache.updating = false; return end
---
---    local signcol_setting = vim.wo.signcolumn
---    local sign_width = 0
---    if signcol_setting == "yes" or signcol_setting == "auto" then
---      sign_width = 2
---    elseif signcol_setting:match("%d") then
---      sign_width = tonumber(signcol_setting)
---    end
---
---    local total_off = wininfo.textoff or 0
---    local num_width = total_off - sign_width
---    if num_width < 0 then num_width = 0 end
---
---    local sign_pad = string.rep(" ", sign_width)
---    local num_pad  = string.rep(" ", num_width)
---
---    local loc = require("nvim-navic").get_location() or ""
---
---    cache.text = string.format("%%#SignColumn#%s%%#LineNr#%s%%*%s", sign_pad, num_pad, loc)
---    cache.updating = false
---
---    -- trigger redraw
---    vim.cmd("redrawstatus")
---  end)
---end
---
---function M.get()
---  -- always schedule an update, but return cached value
---  M.update_async()
---  return cache.text or ""
---end
-
-
 _G.navic_winbar = M
 
-vim.opt.winbar = "%{%v:lua.navic_winbar.get()%}"
 
+-- dont do vim.wo[winid] because will be ugly in vsp
+vim.opt.winbar = "%{%v:lua.navic_winbar.get()%}"
 --vim.opt.winbar = "%{%v:lua.require("nvim-navic").get_location()%}"
 
