2728d9d727b61884e9049a272e4d74fcc854b3e0
acidx27x <telegram@acidx27x>
2025-10-21 10:35:27 +0300

    * nvim: (plugins, dap) init

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -36,6 +36,14 @@ vim.pack.add({
   { src = "https://github.com/stevearc/conform.nvim" },
 })
 require("conform-nvim-config")
+
+
+vim.pack.add({
+  { src = "https://github.com/nvim-neotest/nvim-nio" },
+  { src = "https://github.com/mfussenegger/nvim-dap" },
+  { src = "https://github.com/rcarriga/nvim-dap-ui" },
+})
+require("nvim-dap-ui-config")
 --
 
 
