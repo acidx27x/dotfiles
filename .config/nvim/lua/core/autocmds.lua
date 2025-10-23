4598bfc1c4222b504d85057a06d7675e5e9e610f
acidx27x <telegram@acidx27x>
2025-10-24 01:20:50 +0300

    * nvim: (core, autocmds) remove autocreate dir command interferes oil

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -70,18 +70,6 @@ vim.api.nvim_create_autocmd("VimResized", {
 })
 
 
--- Create directories when saving files
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  callback = function()
-    local dir = vim.fn.expand("<afile>:p:h")
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, "p")
-    end
-  end,
-})
-
-
 -- Save and Load view
 vim.api.nvim_create_autocmd({"BufWinLeave"}, {
   group = augroup,
