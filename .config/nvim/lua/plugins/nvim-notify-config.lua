be857185ae08ae137b9e59105870ccc704c06d2c
acidx27x <telegram@acidx27x>
2025-10-23 10:34:52 +0300

    * nvim: (plugins, notify) change appearence

--- .config/nvim/lua/plugins/nvim-notify-config.lua
+++ .config/nvim/lua/plugins/nvim-notify-config.lua
@@ -1,3 +1,14 @@
 
 vim.notify = require("notify")
 
+
+vim.notify.setup({
+  max_width = 80,
+  stages    = "fade",
+  render    = "wrapped-default",
+})
+
+
+vim.keymap.set({ 'n', 'v' }, "<leader>nd", vim.notify.dismiss,
+  { noremap = true, silent = true, desc = "nvim-notify: dismiss", })
+
