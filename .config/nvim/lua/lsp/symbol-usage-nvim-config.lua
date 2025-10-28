ff8b5aa6ff4ff332f35f91bbdae2d37eca367f0d
kirim <kirim@symica.com>
2025-10-28 12:25:05 +0300

    * nvim: (lsp, symbol-usage) change text format

--- .config/nvim/lua/lsp/symbol-usage-nvim-config.lua
+++ .config/nvim/lua/lsp/symbol-usage-nvim-config.lua
@@ -4,17 +4,50 @@ vim.pack.add({
 })
 
 
-local SymbolKind = vim.lsp.protocol.SymbolKind
+local kind = vim.lsp.protocol.SymbolKind
 
 require("symbol-usage").setup({
   hl = { link = "NonText" },
   vt_position = "end_of_line",  -- above|end_of_line|textwidth|signcolumn
 
-  -- from lsp.SymbolKind https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#symbolKind
-  kinds = { SymbolKind.Function, SymbolKind.Method, SymbolKind.Class, },
+  -- from lsp SymbolKind https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#symbolKind
+  -- kinds = { kind.Function, kind.Method, kind.Class, },
+  kinds = {
+    kind.Interface, kind.Class, kind.Struct, kind.Enum,
+    kind.Constructor, kind.Method, kind.Function,
+  },
 
   references     = { enabled = true, include_declaration = false },
   definition     = { enabled = true, },
   implementation = { enabled = true, },
+
+  request_pending_text = "codelens: loading...",
+  text_format = function(symbol)
+    local fragments = {}
+
+    -- Indicator that shows if there are any other symbols in the same line
+    local stacked_functions = symbol.stacked_count > 0
+        and (" | +%s"):format(symbol.stacked_count)
+        or ""
+
+    if symbol.references and symbol.references > 0 then
+      table.insert(fragments, symbol.references .. " refs")
+    end
+
+    if symbol.definition and symbol.definition > 0 then
+      table.insert(fragments, symbol.definition .. " defs")
+    end
+
+    if symbol.implementation and symbol.implementation > 0 then
+      table.insert(fragments, symbol.implementation .. " impls")
+    end
+
+    local result = table.concat(fragments, ", ") .. stacked_functions
+    if vim.trim(result) ~= "" then
+      result = "codelens: " .. result
+    end
+
+    return result
+  end
 })
 
