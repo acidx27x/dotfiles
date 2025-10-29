42d58df6297ebdc8f37eb5dba988777af25ea39f
kirim <shigolev@is-eda.ru>
2025-10-29 18:52:28 +0300

    * nvim: (plugins, mini) fix hipatterns patterns

--- .config/nvim/lua/plugins/mini-hipatterns-config.lua
+++ .config/nvim/lua/plugins/mini-hipatterns-config.lua
@@ -8,19 +8,18 @@ local hipatterns = require("mini.hipatterns")
 
 
 hipatterns.setup({
-  highlighters = {
-    -- Highlight standalone "FIXME", "HACK", "TODO", "NOTE"
-    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme", },
-    error = { pattern = "%f[%w]()ERROR()%f[%W]", group = "MiniHipatternsFixme", },
+  highlighters = {  -- %w_ ignore words like FATAL_ERROR
+    fixme = { pattern = "%f[%w_]()FIXME()%f[^%w_]", group = "MiniHipatternsFixme", },
+    error = { pattern = "%f[%w_]()ERROR()%f[^%w_]", group = "MiniHipatternsFixme", },
 
-    hack  = { pattern = "%f[%w]()HACK()%f[%W]",  group = "MiniHipatternsHack",  },
-    warn  = { pattern = "%f[%w]()WARN()%f[%W]",  group = "MiniHipatternsHack",  },
+    hack  = { pattern = "%f[%w_]()HACK()%f[^%w_]",  group = "MiniHipatternsHack",  },
+    warn  = { pattern = "%f[%w_]()WARN()%f[^%w_]",  group = "MiniHipatternsHack",  },
 
-    todo  = { pattern = "%f[%w]()TODO()%f[%W]",  group = "MiniHipatternsTodo",  },
+    todo  = { pattern = "%f[%w_]()TODO()%f[^%w_]",  group = "MiniHipatternsTodo",  },
 
-    note  = { pattern = "%f[%w]()NOTE()%f[%W]",  group = "MiniHipatternsNote",  },
-    hint  = { pattern = "%f[%w]()HINT()%f[%W]",  group = "MiniHipatternsNote",  },
-    info  = { pattern = "%f[%w]()INFO()%f[%W]",  group = "MiniHipatternsNote",  },
+    note  = { pattern = "%f[%w_]()NOTE()%f[^%w_]",  group = "MiniHipatternsNote",  },
+    hint  = { pattern = "%f[%w_]()HINT()%f[^%w_]",  group = "MiniHipatternsNote",  },
+    info  = { pattern = "%f[%w_]()INFO()%f[^%w_]",  group = "MiniHipatternsNote",  },
 
     -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = hipatterns.gen_highlighter.hex_color(),
