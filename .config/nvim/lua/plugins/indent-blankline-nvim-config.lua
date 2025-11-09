87cc134c4006852f7de762f59b198dd0412fe850
acidx27x <telegram@acidx27x>
2025-11-09 17:54:51 +0300

    * nvim: (plugins, indent-rainbowline) init

--- .config/nvim/lua/plugins/indent-blankline-nvim-config.lua
+++ .config/nvim/lua/plugins/indent-blankline-nvim-config.lua
@@ -1,22 +1,35 @@
 
 vim.pack.add({
   { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+  { src = "https://github.com/TheGLander/indent-rainbowline.nvim" },
 })
 
 
+require("ibl.highlights").setup()
+local whitespace_hl = require("indent-rainbowline").make_hl_groups({
+  colors = nil,
+  color_transparency = nil,
+  hl = { "IblIndent", "Normal", },
+  prefix = "RainbowColor",
+  auto_setup = true,
+})
+
 require("ibl").setup({
   indent = {
+    -- char = "",
     char = "⎸",
     -- char = "▎",
     -- char = "┃",
   },
 
   whitespace = {
+    highlight = whitespace_hl,
     remove_blankline_trail = false,
   },
 
   scope = {
-    enabled = true,
+    char      = "▎",
+    enabled   = true,
     show_start = false,
     show_end   = false,
     show_exact_scope   = true,
