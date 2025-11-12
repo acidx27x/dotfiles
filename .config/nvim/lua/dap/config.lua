e80bee3d0830694f916eb165c2ea7c1bfcaa2a27
kirim <telegram@acidx27x>
2025-11-12 20:23:05 +0300

    * nvim: (dap) minor changes

--- .config/nvim/lua/dap/config.lua
+++ .config/nvim/lua/dap/config.lua
@@ -1,15 +1,16 @@
 
--- dap plugin group
 require("nvim-dap-config")
-
-require("nvim-dap-virtual-text-config")
-
-require("nvim-dap-ui-config")
---
-
 
 -- dap server group
 require("nvim-dap-codelldb-config")
 require("nvim-dap-lldb-config")
 -- require("nvim-dap-cppdbg-config")
+--
+
+
+-- dap ui group
+require("nvim-dap-ui-config")
+
+require("nvim-dap-virtual-text-config")
+--
 
